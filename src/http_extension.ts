6a920cf41c615d15e275973b268910624d330d15
ElRaptorus <christian.werner@5minds.de>
2018-07-06 12:49:21 +0200

    Stylefixes

--- src/http_extension.ts
+++ src/http_extension.ts
@@ -1,6 +1,6 @@
-import { IIamService, TokenType } from '@essential-projects/core_contracts';
 import {runtime} from '@essential-projects/foundation';
 import {HttpExtension as BaseHttpExtension} from '@essential-projects/http_node';
+import {IIAMService, IIdentity, IIdentityService} from '@essential-projects/iam_contracts';
 import {IMessageBusAdapter} from '@essential-projects/messagebus_contracts';
 import {Container, IInstanceWrapper} from 'addict-ioc';
 
@@ -13,17 +13,20 @@ import * as debug from 'debug';
 import * as helmet from 'helmet';
 import * as http from 'http';
 
-const debugInfo = debug('http_extension:info');
+const debugInfo: debug.IDebugger = debug('http_extension:info');
 
 export class HttpExtension extends BaseHttpExtension {
 
   private _messageBusAdapter: IMessageBusAdapter = undefined;
-  private _iamService: IIamService = undefined;
+  private _iamService: IIAMService = undefined;
   private _httpServer: http.Server = undefined;
 
   public config: any = undefined;
 
-  constructor(container: Container<IInstanceWrapper<any>>, messageBusAdapter: IMessageBusAdapter, iamService: IIamService) {
+  private temporaryRedirectCode: number = 307;
+  private forbiddenErrorCode: number = 403;
+
+  constructor(container: Container<IInstanceWrapper<any>>, messageBusAdapter: IMessageBusAdapter, iamService: IIAMService) {
     super(container);
 
     this._messageBusAdapter = messageBusAdapter;
@@ -38,11 +41,11 @@ export class HttpExtension extends BaseHttpExtension {
     return this._iamService;
   }
 
-  public initializeAppExtensions(app) {
+  public initializeAppExtensions(app: any): void {
     this._httpServer = http.createServer(app);
   }
 
-  public initializeMiddlewareBeforeRouters(app) {
+  public initializeMiddlewareBeforeRouters(app: any): void {
     app.use(busboy());
     app.use(compression());
     const urlEncodedOpts: any = {
@@ -66,27 +69,27 @@ export class HttpExtension extends BaseHttpExtension {
     app.use(helmet.frameguard());
     // https://github.com/helmetjs/x-xss-protection
     app.use(helmet.xssFilter());
-    const csp = this.config.csp;
-    if (csp) {
-      app.use(helmet.contentSecurityPolicy(csp));
+
+    if (this.config.csp) {
+      app.use(helmet.contentSecurityPolicy(this.config.csp));
     }
   }
 
-  private async extractToken(req: any, res: any, next: any) {
+  private async extractToken(req: any, res: any, next: Function): Promise<void> {
       req.token = null;
-      let bearerToken;
-      const bearerHeader = req.headers.authorization;
+      let bearerToken: string;
+      const bearerHeader: string = req.headers.authorization;
 
       // first try auth header
       if (typeof bearerHeader !== 'undefined') {
-          const bearer = bearerHeader.split(' ');
+          const bearer: Array<string> = bearerHeader.split(' ');
           bearerToken = bearer[1];
       } else if (req.cookies.token) {
           // extract token from cookie
           bearerToken = req.cookies.token;
       }
 
-      let context = null;
+      let context: any = null;
       try {
           context = await this.iamService.resolveExecutionContext(bearerToken, TokenType.jwt);
       } catch (err) {
@@ -95,20 +98,20 @@ export class HttpExtension extends BaseHttpExtension {
         // Remove token
         res.cookie('token', '');
 
-        let doRefresh = false;
+        let doRefresh: boolean = false;
         if (this.config.routeConfiguration) {
-          Object.keys(this.config.routeConfiguration).forEach((routeNeedle) => {
-            if (req.url.match(new RegExp('^' + routeNeedle.replace(/\//g, '\\/').replace(/\*/g, '.{0,}') + '$', 'i'))) {
+          Object.keys(this.config.routeConfiguration).forEach((routeNeedle: string) => {
+            if (req.url.match(new RegExp(`^${routeNeedle.replace(/\//g, '\\/').replace(/\*/g, '.{0,}')}$`, 'i'))) {
               doRefresh = this.config.routeConfiguration[routeNeedle].refreshOnInvalidToken;
             }
           });
         }
 
         if (doRefresh) {
-          res.header['Refresh'] = '0;url=' + req.url;
-          res.status(307);
+          res.header['Refresh'] = `0;url=${req.url}`;
+          res.status(this.temporaryRedirectCode);
         } else {
-          res.status(403).json({ error: err.message });
+          res.status(this.forbiddenErrorCode).json({ error: err.message });
         }
       }
 
@@ -119,15 +122,15 @@ export class HttpExtension extends BaseHttpExtension {
   }
 
   public start(): Promise<any> {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve: Function, reject: Function): void => {
       this.messageBusAdapter.start(this._httpServer);
       this._server = this._httpServer.listen(this.config.server.port, this.config.server.host, () => {
 
         runtime.invokeAsPromiseIfPossible(this.onStarted, this)
-          .then((result) => {
+          .then((result: any) => {
             resolve(result);
           })
-          .catch((error) => {
+          .catch((error: Error) => {
             reject(error);
           });
       });
