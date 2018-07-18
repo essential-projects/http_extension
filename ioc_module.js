906a8b4b71965d693846b9eb3faf1d069c4c3354
ElRaptorus <christian.werner@5minds.de>
2018-07-18 12:14:34 +0200

    Fix typo

--- ioc_module.js
+++ ioc_module.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const HttpExtension = require('./dist/commonjs/index').HttpExtension;
-const extensionDiscoveryTag = require('@essential-projects/bootstrapper_contracts').ExtensionDiscoveryTag;
+const extensionDiscoveryTag = require('@essential-projects/bootstrapper_contracts').extensionDiscoveryTag;
 
 function registerInContainer(container) {
 
